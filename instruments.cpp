6fe512cd5dbf0f69c42c067d689a32a9a8920c42
mcstarioni <mcstarioni@live.ru>
2017-05-09 20:59:12 +0300

    Initial commit

---
+++ instruments.cpp
@@ -0,0 +1,6 @@
+#include "instruments.h"
+
+Instruments::Instruments()
+{
+
+}
---
+++ instruments.h
@@ -0,0 +1,11 @@
+#ifndef INSTRUMENTS_H
+#define INSTRUMENTS_H
+
+
+class Instruments
+{
+public:
+    Instruments();
+};
+
+#endif // INSTRUMENTS_H
