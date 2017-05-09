a2a4c0c13fd5a5ed21a40d96ad1ce0074da1779b
mcstarioni <mcstarioni@live.ru>
2017-05-09 21:34:07 +0300

    compleded instruments section

--- instruments.cpp
+++ instruments.cpp
@@ -32,3 +32,22 @@ const bool Plates::membrane = false;
 int Plates::getDiameter(){return this->diameter;}
 bool Plates::hasMembrane(){return Plates::membrane;}
 Plates::~Plates(){}
+
+int Stringed::getStrings(){return this->stringsCount;}
+void Stringed::setStrings(int strings){this->stringsCount = strings;}
+
+const bool Violin::potentiometer = false;
+Bow* Violin::getBow(){return this->bow;}
+void Violin::setBow(Bow *bow){this->bow = bow;}
+bool Violin::hasPotentiometer(){return this->potentiometer;}
+Violin::~Violin(){}
+
+const bool EGuitar::potentiometer = true;
+Pickup* EGuitar::getPickup(){return this->pickup;}
+void EGuitar::setPickup(Pickup *p){this->pickup = pickup;}
+bool EGuitar::hasPotentiometer(){return this->potentiometer;}
+EGuitar::~EGuitar(){}
+
+
+
+
