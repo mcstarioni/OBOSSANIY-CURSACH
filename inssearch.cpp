9e7c3481e8fbc16e28d14dabd59b9dd8034579fb
mcstarioni <mcstarioni@live.ru>
2017-06-29 17:43:39 +0300

    ЗАЕБАЛООО

--- inssearch.cpp
+++ inssearch.cpp
@@ -7,13 +7,15 @@ InsSearch::InsSearch(bool final,QWidget *parent) :
 {
     groupSearch = 0;
     ui->setupUi(this);
-    isCorrect = true;
-    classValue = -1;
-    classType = 3;
+    freq = new Search::SearchArgument<int>(0,notImportant);
+    id = new Search::SearchArgument<int>(0,notImportant);
+    name = new Search::SearchArgument<QString>("",notImportant);
+    classes = new Search::SearchArgument<int>(0,notImportant);
     if(final)
     {
         ui->groupSearchButton->hide();
     }
+    inputCorrect
 }
 
 InsSearch::~InsSearch()
@@ -24,19 +26,17 @@ InsSearch::~InsSearch()
 void InsSearch::on_comboBox_currentIndexChanged(int index)
 {
     if(index == 0)
-        classType = 3;
+        classes.comparisonType = notImportant;
     else
-        classType = 0;
-    classValue = index - 1;
+        classes.comparisonType = equal;
+    classes.value = index - 1;
 }
 
 void InsSearch::on_idEdit_editingFinished()
 {
-    QString temp = ui->idEdit->text();
-    parseString(&idType,&temp);
-    if(idType == -1)
-        isCorrect = false;
-    idValue = temp.toInt(&isCorrect);
+    int result = ui->idEdit->text().toInt();
+    if()
+    id.value =
 }
 
 void InsSearch::on_nameEdit_editingFinished()
@@ -49,78 +49,12 @@ void InsSearch::on_nameEdit_editingFinished()
 
 void InsSearch::on_freqEdit_editingFinished()
 {
-    QString temp = ui->freqEdit->text();
-    parseString(&freqType,&temp);
-    if(freqType == -1)
-        isCorrect = false;
-    freqValue = temp.toInt(&isCorrect);
 
 }
 
 void InsSearch::search()
 {
 
-}
-QVector<Instruments*>* InsSearch::find(QVector<Instruments*>* instruments)
-{
-    QVector<Instruments*>* result = new QVector<Instruments*>();
-    for(int i = 0; i < instruments->size(); i++)
-    {
-        if(checkConditions(instruments->at(i)))
-        {
-            result->push_back(instruments->at(i));
-        }
-    }
-    return result;
-}
-void InsSearch::parseString(int *searchType, QString* searchValue)
-{
-    QChar type = searchValue->at(0);
-    *searchType = (type == '=')?0:
-                ((type == '<')?1:
-                ((type == '>')?2:
-                 (type == '?')?3:-1));
-    if(*searchType == -1)
-    {
-        return;
-    }
-    *searchValue = searchValue->right(searchValue->length()-1);
-
-}
-bool InsSearch::checkConditions(Instruments* ins)
-{
-    return (checkOneCondition(idType,ins->Id(),idValue) &&
-            checkOneCondition(freqType,ins->getFrequency(),freqValue) &&
-            checkOneCondition(nameType,ins->getName(),nameValue) &&
-            checkOneCondition(classType,ins->Type(),classValue)
-            );
-}
-template <class T>
-bool InsSearch::checkOneCondition(enum comparisonType, T value1,T value2)
-{
-    switch(comparisonType)
-    {
-        case 0:
-        {
-            return (value1 == value2);
-        }
-        case 1:
-        {
-            return (value1 < value2);
-        }
-        case 2:
-        {
-            return (value1 > value2);
-        }
-        case 3:
-        {
-            return true;
-        }
-        case -1:
-        {
-            return false;
-        }
-    }
 }
 
 void InsSearch::on_groupSearchButton_toggled(bool checked)
